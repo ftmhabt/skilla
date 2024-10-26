8db99e53d15393d8a8cfa226483bd6b5dd39c6a5
ftmhabt <ftmh.a78@gmail.com>
2024-10-26 13:01:30 +0330

    Fix logout

--- app/components/logout.tsx
+++ app/components/logout.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 export default function Logout() {
-  const router = useRouter();
+  // const router = useRouter();
   const signout = () => {
-    signOut({ redirect: false }).then(() => {
-      router.push("/");
-    });
+    signOut();
   };
   return <Button onClick={signout}>logout</Button>;
 }
