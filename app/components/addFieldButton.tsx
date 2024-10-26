76a25d2f80b2b7d51974dae75dc7e554ae2b80df
ftmhabt <ftmh.a78@gmail.com>
2024-10-26 13:01:45 +0330

    Add fields components

---
+++ app/components/addFieldButton.tsx
@@ -0,0 +1,3 @@
+export default function addFieldButton() {
+  return <div className="bg-slate-400 text-white rounded-full text-2xl">+</div>;
+}
---
+++ app/components/fieldsContainer.tsx
@@ -0,0 +1,3 @@
+export default function fieldsContainer() {
+  return <div></div>;
+}
