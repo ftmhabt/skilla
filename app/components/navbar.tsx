9725236ab8dc1a821c961e11a0ce329f81a68f96
ftmhabt <ftmh.a78@gmail.com>
2024-10-31 16:05:48 +0330

    Remove avatar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,3 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 import Link from "next/link";
 import { ModeToggle } from "../../components/mode-toggle";
 
@@ -15,26 +5,6 @@ const Navbar = () => {
   return (
     <nav className="bg-primary dark:bg-slate-700 text-white">
       <div className="py-2 container mx-auto px-5 flex justify-between items-center">
-        <div className="flex items-center">
-          <DropdownMenu>
-            <DropdownMenuTrigger className="focus:outline-none">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>💀</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Link href="/profile">Profile</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href="/auth">Logout</Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
         <ModeToggle />
         <Link href={"/"} className="text-3xl">
           skilla
