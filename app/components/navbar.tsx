93ef695e916e394502f19041808c6c60ac0b6513
ftmhabt <ftmh.a78@gmail.com>
2024-11-01 20:53:52 +0330

    Add logo

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { ModeToggle } from "../../components/mode-toggle";
-// import Image from "next/image";
-// import Logo from "../img/logo.png";
+import Image from "next/image";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
   return (
@@ -9,8 +9,13 @@ const Navbar = () => {
       <div className="flex py-2 container mx-auto px-5 justify-between items-center sm:mx-auto sm:w-[400px]">
         <ModeToggle />
         <Link href={"/"} className="text-3xl self-end justify-self-end">
-          {/* <Image src={Logo} alt="logo" width={80} height={30} className="p-1" /> */}
-          skilla
+          <Image
+            src={Logo}
+            alt="skilla logo"
+            width={80}
+            height={30}
+            className="p-1"
+          />
         </Link>
       </div>
     </nav>
