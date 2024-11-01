643d54f61d8fc36118f96723f68bbc854a50c3f5
ftmhabt <ftmh.a78@gmail.com>
2024-11-01 21:59:17 +0330

    Tidy up

--- prisma/migrations/20241028195132_make_user_id_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Skill" DROP CONSTRAINT "Skill_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Skill" ALTER COLUMN "userId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Skill" ADD CONSTRAINT "Skill_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
